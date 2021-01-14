3049d2ccf6e5653791f2015c11be4418030757c0
Wicked441 <74775601+Wicked441@users.noreply.github.com>
2021-01-14 16:55:07 +0100

    Update SR04.py

--- SR04.py
+++ SR04.py
@@ -1,55 +1,29 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode( GPIO.BCM )
-GPIO.setwarnings( 0 )
- 
-#set GPIO Pins
-GPIO_TRIGGER = 20
-GPIO_ECHO = 21
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-
-def distance():
-    # set Trigger to HIGH
+def afstand():
+    
+    #True en False zijn andere woorden voor HIGH en LOW
+    #Code werkt niet als HIGH of LOW worden geschreven
     GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
+    starttijd = time.time()
+    stoptijd = time.time()
+
     while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
+        starttijd = time.time()
     while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+        stoptijd = time.time()
+
+    t = stoptijd - starttijd
+    v = 34300 #snelheid van geluid door de lucht in cm/s
+    s = (t * v) / 2
  
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+    return s
+
+while True:
+    s = afstand()
+    print ("gemeten afstand =" % s)
+    time.sleep(1)
              
-            if dist < 100:
-                print("Speler is online")
-            else:
-                print ("Speler is offline")
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+    if s < 100:
+        print("Speler is online")
+    else:
+        print ("Speler is offline")
